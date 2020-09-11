b881b8b87fe68ca31c23f83e17f2a1eeadbef855
dddaaammmooo <dddaaammmooo@users.noreply.github.com>
2020-09-11 14:29:52 +1000

    You miss one bloody quotation mark...

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ set -eo pipefail
 # If a Github token was supplied use that
 if [[ ! -z "${DOCKER_BASE_IMAGE_REPOSITORY}" && ! -z "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME}" && ! -z "${DOCKER_BASE_IMAGE_REPOSITORY_PASSWORD}" && ! -z "${DOCKER_BASE_IMAGE}" ]]; then 
   echo "Authenticating: ${DOCKER_BASE_IMAGE_REPOSITORY}..."
-  docker login ${DOCKER_BASE_IMAGE_REPOSITORY} \
-    --username "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME} \
+  docker login "${DOCKER_BASE_IMAGE_REPOSITORY}" \
+    --username "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME}" \
     --password "${DOCKER_BASE_IMAGE_REPOSITORY_PASSWORD}";
   echo "Pulling: ${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}..."
-  docker pull ${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}
+  docker pull "${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}"
 fi
 
 # Authenticate to ECR
