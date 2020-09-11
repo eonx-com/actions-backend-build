fa0d9ea115c494885187d75b1084de8b8a6d62ba
dddaaammmooo <dddaaammmooo@users.noreply.github.com>
2020-09-11 14:24:40 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,13 @@
 set -eo pipefail
 
 # If a Github token was supplied use that
-if [[ ! -z "${DOCKER_GITHUB_TOKEN}" ]]; then 
-  export GITHUB_TOKEN="${DOCKER_GITHUB_TOKEN}";
+if [[ ! -z "${DOCKER_BASE_IMAGE_REPOSITORY}" && ! -z "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME}" && ! -z "${DOCKER_BASE_IMAGE_REPOSITORY_PASSWORD}" && ! -z "${DOCKER_BASE_IMAGE}" ]]; then 
+  echo "Authenticating: ${DOCKER_BASE_IMAGE_REPOSITORY}..."
+  docker login ${DOCKER_BASE_IMAGE_REPOSITORY} \
+    --username "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME} \
+    --password "${DOCKER_BASE_IMAGE_REPOSITORY_PASSWORD}";
+  echo "Pulling: ${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}..."
+  docker pull ${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}
 fi
 
 # Authenticate to ECR
